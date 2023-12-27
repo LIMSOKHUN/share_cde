3eed659aa7b9509458a88e17563d66bdbd3f004a
LIMSOKHUN <154854106+LIMSOKHUN@users.noreply.github.com>
2023-12-27 19:25:53 +0700

    Add files via upload

---
+++ share_code/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/css/bootstrap.min.css" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Select Data</title>
+</head>
+
+<body class="d-flex vw-100 vh-100 align-items-center justify-content-center">
+    <div class="card" style="width: 80rem;">
+        <div class="card-body">
+            <div class="section-header d-flex justify-content-between">
+                <h5 class="card-title text-uppercase">Select Data</h5>
+                <a class="btn btn-primary btn-small" href="insert.php">Add Data</a>
+            </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">គោត្តនាម និង​​នាម</th>
+                        <th scope="col">អក្សរឡាតាំង</th>
+                        <th scope="col">អ៊ីម៉ែល</th>
+                        <th scope="col">ភេទ</th>
+                        <th scope="col">ថ្ងៃ ខែ ឆ្នាំ កំណើត</th>
+                        <th scope="col">ទីកន្លែងកំណើត</th>
+                        <th scope="col">មុខតំណែង</th>
+                        <th scope="col">លេខទូរស័ព្ទ</th>
+                        <th scope="col">Status</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>លីម សុខខុន</td>
+                        <td>lim sokhun</td>
+                        <td>sokhunlim36@gmail.com</td>
+                        <td>ប្រុស</td>
+                        <td>12/12/2003</td>
+                        <td>កំពង់ស្ពឺ</td>
+                        <td>គ្រូបង្រៀន</td>
+                        <td>087288682</td>
+                        <td>
+                            <a href="edit.php">Edit</a>
+                            <a href="">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>លីម សុខខុន</td>
+                        <td>lim sokhun</td>
+                        <td>sokhunlim36@gmail.com</td>
+                        <td>ប្រុស</td>
+                        <td>12/12/2003</td>
+                        <td>កំពង់ស្ពឺ</td>
+                        <td>គ្រូបង្រៀន</td>
+                        <td>087288682</td>
+                        <td>
+                            <a href="edit.php">Edit</a>
+                            <a href="">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>លីម សុខខុន</td>
+                        <td>lim sokhun</td>
+                        <td>sokhunlim36@gmail.com</td>
+                        <td>ប្រុស</td>
+                        <td>12/12/2003</td>
+                        <td>កំពង់ស្ពឺ</td>
+                        <td>គ្រូបង្រៀន</td>
+                        <td>087288682</td>
+                        <td>
+                            <a href="edit.php">Edit</a>
+                            <a href="">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>លីម សុខខុន</td>
+                        <td>lim sokhun</td>
+                        <td>sokhunlim36@gmail.com</td>
+                        <td>ប្រុស</td>
+                        <td>12/12/2003</td>
+                        <td>កំពង់ស្ពឺ</td>
+                        <td>គ្រូបង្រៀន</td>
+                        <td>087288682</td>
+                        <td>
+                            <a href="edit.php">Edit</a>
+                            <a href="">Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js">
+    </script>
+
+</body>
+
+</html>
